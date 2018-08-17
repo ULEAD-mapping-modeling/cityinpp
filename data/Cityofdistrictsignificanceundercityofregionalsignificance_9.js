d0e9013a35ac22d6171a3ad18ca5147c7dee945a
Stanislav <ostapenkostas@gmail.com>
2018-08-17 12:53:11 +0300

    q1

---
+++ data/Cityofdistrictsignificanceundercityofregionalsignificance_9.js
@@ -0,0 +1,24 @@
+var json_Cityofdistrictsignificanceundercityofregionalsignificance_9 = {
+"type": "FeatureCollection",
+"name": "Cityofdistrictsignificanceundercityofregionalsignificance_9",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "nameUA_new": "Білицьке", "variant": "1" }, "geometry": { "type": "Point", "coordinates": [ 37.183083959439607, 48.406655312856635 ] } },
+{ "type": "Feature", "properties": { "nameUA_new": "Білозерське", "variant": "1" }, "geometry": { "type": "Point", "coordinates": [ 37.057213764525962, 48.535447457724359 ] } },
+{ "type": "Feature", "properties": { "nameUA_new": "Родинське", "variant": "1" }, "geometry": { "type": "Point", "coordinates": [ 37.208707610025556, 48.353845500318847 ] } },
+{ "type": "Feature", "properties": { "nameUA_new": "Соснівка", "variant": "2" }, "geometry": { "type": "Point", "coordinates": [ 24.250741164743058, 50.293471937777042 ] } },
+{ "type": "Feature", "properties": { "nameUA_new": "Гірник", "variant": "1" }, "geometry": { "type": "Point", "coordinates": [ 37.369565673406385, 48.052985209387565 ] } },
+{ "type": "Feature", "properties": { "nameUA_new": "Святогірськ", "variant": "1" }, "geometry": { "type": "Point", "coordinates": [ 37.569145844772393, 49.040276130722255 ] } },
+{ "type": "Feature", "properties": { "nameUA_new": "Стебник", "variant": "2" }, "geometry": { "type": "Point", "coordinates": [ 23.550958664634013, 49.298900486028735 ] } },
+{ "type": "Feature", "properties": { "nameUA_new": "Світлодарськ", "variant": "1" }, "geometry": { "type": "Point", "coordinates": [ 38.220982347480451, 48.433660854801282 ] } },
+{ "type": "Feature", "properties": { "nameUA_new": "Часів Яр", "variant": "1" }, "geometry": { "type": "Point", "coordinates": [ 37.838145383182521, 48.585322096202425 ] } },
+{ "type": "Feature", "properties": { "nameUA_new": "Новодружеськ", "variant": "1" }, "geometry": { "type": "Point", "coordinates": [ 38.347497404722162, 48.965912557864904 ] } },
+{ "type": "Feature", "properties": { "nameUA_new": "Щастя", "variant": "1" }, "geometry": { "type": "Point", "coordinates": [ 39.228393246628713, 48.737807418044383 ] } },
+{ "type": "Feature", "properties": { "nameUA_new": "Привілля", "variant": "1" }, "geometry": { "type": "Point", "coordinates": [ 38.300914773732217, 49.007833183717288 ] } },
+{ "type": "Feature", "properties": { "nameUA_new": "Гірське", "variant": "1" }, "geometry": { "type": "Point", "coordinates": [ 38.499458735396814, 48.748859158186455 ] } },
+{ "type": "Feature", "properties": { "nameUA_new": "Таврійськ", "variant": "1" }, "geometry": { "type": "Point", "coordinates": [ 33.42826204798871, 46.753878153377833 ] } },
+{ "type": "Feature", "properties": { "nameUA_new": "Винники", "variant": "2" }, "geometry": { "type": "Point", "coordinates": [ 24.138808228222967, 49.813839127205291 ] } },
+{ "type": "Feature", "properties": { "nameUA_new": "Українськ", "variant": "2" }, "geometry": { "type": "Point", "coordinates": [ 37.363214501801345, 48.094759679400823 ] } },
+{ "type": "Feature", "properties": { "nameUA_new": "Залізне", "variant": "1" }, "geometry": { "type": "Point", "coordinates": [ 37.894317859930908, 48.366834332129962 ] } }
+]
+}
